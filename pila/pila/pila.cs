293fcf7aed6b1c0b3b05024686bfc2bc671f0dcb
Alecas280100 <castritoale@hotmail.com>
2019-02-27 08:35:58 -0400

    pila

---
+++ pila/pila/pila.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pila
+{
+    class pila
+    {
+        public static int max = 10;
+        private int[] vector = new int[max];
+        private int tope = -1;
+
+        public void push(int n)
+        {
+            if (!llena())
+            {
+                tope++;
+                vector[tope] = n;
+            }
+            else
+                Console.WriteLine("La pila esta llena");
+        }
+        public int pop()
+        {
+            if (!vacia())
+            {
+                tope--;
+                return vector[tope + 1];
+            }
+            else
+                Console.WriteLine("La pila esta vacia");
+            return -1;
+        }
+        private Boolean vacia()
+        {
+            return tope == -1;
+        }
+        private Boolean llena()
+        {
+            return tope == max;
+        }
+        public void mostrar()
+        {
+            for(int i=0;i<=tope;i++)
+            {
+                Console.WriteLine("{0}", vector[i]);
+            }
+        }
+    }
+}
+
