cf94af9c09cbb7c239892b1837d35c4440341161
Alecas280100 <castritoale@hotmail.com>
2019-03-06 08:20:59 -0400

    Circular

---
+++ cola circular/cola circular/Circular.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cola_circular
+{
+    class Circular
+    {
+        public const int maximo = 10;
+        private int[] arreglo = new int[maximo];
+        private int frente = 0;
+        private int final = 0;
+
+        public void insertar(int valor)
+        {
+            if (!llena())
+            {
+                arreglo[final] = valor;
+                final = (final + 1) % maximo;
+            }
+            else
+                Console.WriteLine("Cola llena");
+        }
+        public int Eliminar()
+        {
+            if (!vacia())
+            {
+                int valor;
+                valor = arreglo[frente];
+                frente = (frente + 1) % maximo;
+                return valor;
+            }
+            else
+                Console.WriteLine("Cola vacia");
+            return arreglo[frente];
+        }
+        public void mostrar()
+        {
+            int aux = frente;
+            while(aux!=final)
+            {
+                Console.Write(arreglo[aux] + " ");
+                aux = (aux + 1) % maximo;
+            }
+        }
+
+        private Boolean llena()
+        {
+            return final == (final + 1) % maximo;
+        }
+
+        private Boolean vacia()
+        {
+            return frente == final;
+        }
+    }
+}
---
+++ cola circular/cola circular/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cola_circular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean salir = false;
+            Circular cir = new Circular();
+            int opcion = 0;
+            int x = 0;
+            while (salir != true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Insertar");
+                Console.WriteLine("2. Eliminar");
+                Console.WriteLine("3. Mostrar");
+                Console.WriteLine("4. Salir");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        x = int.Parse(Console.ReadLine());
+                        cir.insertar(x);
+                        break;
+                    case 2:
+                        x = cir.Eliminar();
+                        Console.WriteLine("El elemento eliminado es:" + x);
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        cir.mostrar();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                }
+            }
+        }
+    }
+}
